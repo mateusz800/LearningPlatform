dee469126c18f0fb1956e6528f44d4be0e3c67ec
Mateusz Budnik <mat.budnik@outlook.com>
2020-12-20 18:30:55 +0100

    setting up service discovery

--- apiGateway/src/main/java/com/example/apiGateway/RouteConfiguration.java
+++ apiGateway/src/main/java/com/example/apiGateway/RouteConfiguration.java
@@ -1,18 +1,20 @@
 package com.example.apiGateway;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableDiscoveryClient
 public class RouteConfiguration {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("test", r ->
                         r.path("/auth/test")
-                                .uri("http://192.168.1.69:8125/"))
+                                .uri("lb://account-service"))
                 .build();
     }
 
--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo -e "\n Building Account Servcie"
+echo -e "\n Building Account Service"
 cd accountService && ./gradlew build && cd ..
 echo -e "\n Building API Gateway"
 cd apiGateway && ./gradlew build && cd ..
+echo -e "\n Building Service Discovery"
+cd serviceDiscovery && ./gradlew build && cd ..
